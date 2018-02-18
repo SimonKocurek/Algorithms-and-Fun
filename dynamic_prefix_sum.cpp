e214d280b26319a0c56edc1d4ef0b326c592e058
justmesr <simon.kocurek@gmail.com>
2018-02-18 09:49:31 +0100

    Binary Index Tree

--- dynamic_prefix_sum.cpp
+++ dynamic_prefix_sum.cpp
@@ -4,45 +4,84 @@
 
 using namespace std;
 
-auto load_input() {
+vector<int> values;
+vector<long> tree;
+
+void load_values() {
     int size;
     cin >> size;
 
-    vector<int> result(size);
-    for (int i = 0; i < size; ++i) {
+    values = vector<int>(size + 1);
+    for (int i = 1; i < size + 1; ++i) {
         int value;
         cin >> value;
 
-        result[i] = value;
+        values[i] = value;
+    }
+}
+
+int lowest_one_bit(int number) {
+    return number & (-number); 
+}
+
+long prefix_sum(int index) {
+    long sum = 0;
+
+    while (index > 0) {
+        sum += tree[index];
+        index -= lowest_one_bit(index);
     }
 
-    return result;
+    return sum;
 }
 
-auto create_fenwick_tree(vector<int> &values) {
-    vector<int> result(values.size());
-
-    return result;
+void print(int start, int end) {
+    cout << prefix_sum(end) - prefix_sum(start - 1) << "\n";
 }
 
-void execute_print(vector<int> &tree) {
+void change(int index, int value) {
+    int change = value - values[index];
+    values[index] = value;
 
+    while (index < tree.size()) {
+        tree[index] += change;
+        index += lowest_one_bit(index);
+    }
 }
 
-void execute_change(vector<int> &tree) {
+void create_fenwick_tree() {
+    tree = vector<long>(values.size());
+
+    for (int i = 1; i < tree.size(); ++i) {
+        tree[i] = values[i] + prefix_sum(i - 1) - prefix_sum(i - lowest_one_bit(i));
+    }
 }
 
-void process_instructions(vector<int> &tree) {
+void execute_print() {
+    int start, end;
+    cin >> start >> end;
+
+    print(start, end);
+}
+
+void execute_change() {
+    int index, value;
+    cin >> index >> value;
+
+    change(index, value);
+}
+
+void process_instructions() {
     while (true) {
         int instruction_type;
         cin >> instruction_type;
 
         switch(instruction_type) {
             case 2:
-                execute_print(tree);
+                execute_print();
                 break;
             case 1:
-                execute_change(tree);
+                execute_change();
                 break;
             case 0:
                 return;
@@ -51,8 +90,8 @@ void process_instructions(vector<int> &tree) {
 }
 
 int main() {
-    auto values = load_input();
-    auto tree = create_fenwick_tree(values);
+    load_values();
+    create_fenwick_tree();
     process_instructions();
     return 0;
 }
