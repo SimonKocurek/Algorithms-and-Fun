25c8464e6e9a92b362e03726f1e9caeccc75d715
justmesr <simon.kocurek@gmail.com>
2018-02-19 14:40:08 +0100

    Interval tree

--- baguette.cpp
+++ baguette.cpp
@@ -4,41 +4,95 @@
 
 using namespace std;
 
-void add_cheese(vector<int> &cheese) {
-    int start, end;
-    cin >> start >> end;
-    
-    cheese[start]++;
-    if (end + 1 < cheese.size()) {
-        cheese[end + 1]--;
+struct tree {
+    int start;
+    int end;
+    long value = 0;
+    tree *left;
+    tree *right;
+
+    tree(int start, int end) : start(start), end(end) {}
+};
+
+void add_layer(tree *values, int start, int end) {
+    if (start == values->start && end == values->end) {
+        values->value++;
+        return;
     }
+
+    if (start >= values->left->start && end <= values->left->end) {
+        add_layer(values->left, start, end);
+        return;
+    }
+
+    if (start >= values->right->start && end <= values->right->end) {
+        add_layer(values->right, start, end);
+        return;
+    }
+
+    add_layer(values->left, start, values->left->end);
+    add_layer(values->right, values->right->start, end);
 }
 
-void print_cheese(vector<int> &cheese) { 
+void add_cheese(tree *values) {
+    int start, end;
+    cin >> start >> end;
+
+    add_layer(values, start, end);
+}
+
+long get_overlaps(tree *values, int index) {
+    long sum = 0;
+
+    auto current = values;
+    while(current->start != index || current->end != index) {
+        sum += current->value;
+
+        if (index <= current->left->end) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    return sum + current->value;
+}
+
+void print_cheese(tree *values) { 
     int printed;
     cin >> printed;
 
-    int current = 0;
-    for (int i = 1; i <= printed; ++i) {
-        current += cheese[i];
+    cout << get_overlaps(values, printed) << '\n';
+}
+
+tree *initialize_tree(int start, int end) {
+    tree *values = new tree(start, end);
+
+    if (start == end) {
+        return values;
     }
 
-    cout << current << '\n';
+    int middle = (start + end) / 2;
+
+    values->left = initialize_tree(start, middle);
+    values->right = initialize_tree(middle + 1, end);
+
+    return values;
 }
 
 int main() {
     int size;
     cin >> size;
-    
-    vector<int> cheese(size + 1);
-    
+
+    tree *values = initialize_tree(1, size);
+
     int command;
     cin >> command;
     while (command != 0) {
         if (command == 1) {
-            add_cheese(cheese);
+            add_cheese(values);
         } else {
-            print_cheese(cheese);
+            print_cheese(values);
         }
         cin >> command;
     }
