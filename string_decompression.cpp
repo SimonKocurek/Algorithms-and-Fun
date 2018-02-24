950561e7c428e0610c872526101d6081cb52c821
justmesr <simon.kocurek@gmail.com>
2018-02-24 16:48:53 +0100

    Cleanup

---
+++ string_decompression.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string _compressed;
+
+string decompress(const string &compressed) {
+    _compressed = compressed;
+    string result;
+
+    return result;
+}
+
+int main() {
+    return 0;
+}
+
