5bbc0162a2c7d3e5e1ddbb7822d4678e4de73617
justmesr <simon.kocurek@gmail.com>
2018-03-01 20:30:30 +0100

    String decompression

--- decompress.cpp
+++ decompress.cpp
@@ -2,14 +2,77 @@
 
 using namespace std;
 
+bool is_digit(char c);
+
+int parse_number(string::iterator &start);
+
+string parse_sub_expression(string::iterator &start);
+
+string decompress_sub_expression(string::iterator &start);
+
+char decompress_character(string::iterator &start);
+
+bool is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+int parse_number(string::iterator &start) {
+    auto number = 0;
+
+    while (*start != '[') {
+        number *= 10;
+        number += *start - '0';
+        start++;
+    }
+
+    return number;
+}
+
+string parse_sub_expression(string::iterator &start) {
+    start++;
+    string result;
+
+    while (*start != ']') {
+        if (is_digit(*start)) {
+            result += decompress_sub_expression(start);
+        } else {
+            result += decompress_character(start);
+        }
+    }
+
+    start++;
+
+    return result;
+}
+
+string decompress_sub_expression(string::iterator &start) {
+    string result;
+
+    auto digit = parse_number(start);
+    auto repeated = parse_sub_expression(start); 
+
+    for (auto i = 0; i < digit; ++i) {
+        result += repeated;
+    }
+
+    return result;
+}
+
+char decompress_character(string::iterator &start) {
+    auto c = *start;
+    start++;
+    return c;
+}
+
 string decompress(string::iterator start, string::iterator end) {
     string result;
 
-    while (start < end) {
-        auto c = *start;
-
-        if (c <= '0' && c <= '9') {
-            result += parse_number(
+    while (start !=  end) {
+        if (is_digit(*start)) {
+            result += decompress_sub_expression(start);
+        } else {
+            result += decompress_character(start);
+        }
     }
 
     return result;
@@ -34,7 +97,7 @@ int main() {
 
     cout << decompress("10[a]") << "\n";
     cout << "aaaaaaaaaa" << "\n\n";
-    
+
     return 0;
 }
 
