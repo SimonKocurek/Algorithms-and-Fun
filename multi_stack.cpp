01e84aa2b58b8a06d4da8ce829b46f0df41c1fdf
justmesr <simon.kocurek@gmail.com>
2018-03-07 15:46:16 +0100

    Multi stack with pop at

--- multi_stack.cpp
+++ multi_stack.cpp
@@ -5,7 +5,7 @@ using namespace std;
 template<class T>
 class multi_stack {
     private:
-        vector<stack<T>> stacks;
+        vector<vector<T>> stacks;
         uint32_t threshold;
 
     public:
@@ -13,10 +13,10 @@ class multi_stack {
 
         void push(T value) {
             if (!stacks.empty() && stacks.back().size() < threshold) {
-                stacks.back().push(value);
+                stacks.back().push_back(value);
+
             } else {
-                stack<T> new_stack;
-                new_stack.push(value);
+                vector<T> new_stack {value};
                 stacks.push_back(new_stack);
             }
         }
@@ -26,8 +26,8 @@ class multi_stack {
                 throw;
             }
 
-            auto result = stacks.back().top();
-            stacks.back().pop();
+            auto result = stacks.back().back();
+            stacks.back().pop_back();
 
             if (stacks.back().empty()) {
                 stacks.pop_back();
@@ -36,7 +36,41 @@ class multi_stack {
             return result;
         }
 
-        T pop_at(int stack_idx) {
+        T pop_at(uint32_t stack_idx) {
+            if (stacks.empty() || stack_idx < 0 || stack_idx + 1 > stacks.size()) {
+                throw;
+            }
+
+            auto result = stacks[stack_idx].back();
+            stacks[stack_idx].pop_back();
+
+            while (stack_idx + 1 < stacks.size() && stacks[stack_idx].size() < threshold) {
+                auto &current = stacks[stack_idx];
+                auto &next = stacks[stack_idx + 1];
+
+                auto value = next.front();
+                next.erase(next.begin());
+                current.push_back(value);
+
+                if (next.empty()) {
+                    stacks.pop_back();
+                }
+
+                stack_idx++;
+            }
+
+            return result;
+        }
+
+        void print() {
+            cout << '\n' << "Stack:" << '\n';
+            for (auto sub_stack : stacks) {
+                for (auto value : sub_stack) {
+                    cout << value << " ";
+                }
+                cout << '\n';
+            }
+            cout << '\n';
         }
 };
 
@@ -59,6 +93,28 @@ int main() {
 
     cout << test.pop() << "\n";
     cout << test.pop() << "\n";
+
+    test.print();
+
+    multi_stack<int> test2(2);
+    test2.push(1);
+    test2.push(2);
+    test2.push(3);
+    test2.push(4);
+    test2.push(5);
+
+    cout << test2.pop_at(0) << "\n";
+    cout << test2.pop_at(1) << "\n";
+    cout << test2.pop_at(0) << "\n";
+    cout << test2.pop() << "\n";
+
+    test2.push(6);
+    test2.push(7);
+    test2.push(8);
+
+    test2.print();
+    cout << test2.pop_at(1) << "\n";
+    cout << test2.pop_at(0) << "\n";
 
     return 0;
 }
