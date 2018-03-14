56c5968713f512dc1adb744fbd7bcfd4223cdd84
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-03-14 12:21:42 +0100

    Improved permutation search

--- permutation_is_palindrome.py
+++ permutation_is_palindrome.py
@@ -6,15 +6,8 @@ def can_be_palindrome(s):
     for char in s:
         char_odd ^= 1 << (ord(char) - ord('a')) 
     
-    can_be_odd = True
-    for i in range(27):
-        if char_odd & (1 << i) > 0:
-            if can_be_odd:
-                can_be_odd = False
-            else:
-                return False
-
-    return True
+    char_odd_ones = char_odd - 1
+    return char_odd & char_odd_ones == 0
 
 s = input()
 print(can_be_palindrome(s))
