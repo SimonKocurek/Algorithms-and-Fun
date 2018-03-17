14bb61458caf68527d5574b806b11953ffba60da
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-03-17 22:20:44 +0100

    Largest rectangle in histogram

--- largest-rectangle-in-histrogram.cpp
+++ largest-rectangle-in-histrogram.cpp
@@ -2,19 +2,49 @@
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
-pii largest_rectangle(vector<int> &histogram)
+int largest_rectangle(vector<int> &histogram)
 {
-    if (histogram.size() <= 1)
+    int result = 0;
+    stack<int> visited;
+
+    for (int i = 0; i < histogram.size(); ++i)
     {
-        return {0, 0};
+        if (visited.empty() || histogram[visited.top()] <= histogram[i])
+        {
+            visited.push(i);
+        }
+        else
+        {
+            while (histogram[visited.top()] > histogram[i])
+            {
+                int area = histogram[visited.top()] * (i - visited.top());
+                visited.pop();
+
+                result = max(result, area);
+            }
+
+            --i;
+        }
     }
 
-    
+    while (!visited.empty())
+    {
+        int area = histogram[visited.top()] * (histogram.size() - visited.top());
+        visited.pop();
+
+        result = max(result, area);
+    }
+
+    return result;
 }
 
 int main()
 {
+    vector<int> a{1, 1, 2, 3, 4, 3, 1, 2, 2};
+    vector<int> b{1, 2, 3, 4, 5, 3, 3, 2};
+
+    cout << largest_rectangle(a) << "\n";
+    cout << largest_rectangle(b) << "\n";
+
     return 0;
 }
