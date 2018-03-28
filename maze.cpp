34f27cbeb9f9263b77020690c81931b9851f8306
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-03-28 10:27:14 +0200

    Maze movement

--- maze.cpp
+++ maze.cpp
@@ -3,9 +3,56 @@
 using namespace std;
 
 typedef vector<vector<bool>> maze;
+typedef vector<vector<int>> maze_distances;
 typedef pair<int, int> point;
 
-int minimum_steps(maze& board, point& start, point& end) {
+const point directions[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+bool valid(int x, int y, int max_x, int max_y) {
+    return x >= 0 && x < max_x &&
+        y >= 0 && y < max_y;
+}
+
+int minimum_steps(maze& board, point start, point end) {
+    if (start == end) {
+        return 0;
+    }
+
+    maze visited(board.size(), vector<bool>(board[0].size(), false));
+    maze_distances distance(board.size(), vector<int>(board[0].size()));
+
+    visited[start.first][start.second] = true;
+    distance[start.first][start.second] = 0;
+
+    queue<point> points;
+    points.push(start);
+
+    while (!points.empty()) {
+        auto current = points.front();
+        points.pop();
+
+        auto new_distance = distance[current.first][current.second] + 1;
+        for (auto& direction : directions) {
+            auto new_y = current.first + direction.first;
+            auto new_x = current.second + direction.second;
+
+            if (valid(new_x, new_y, board[0].size(), board.size()) && 
+                    !visited[new_y][new_x] && 
+                    !board[new_y][new_x]) {
+                point new_point = {new_y, new_x};
+
+                if (new_point == end) {
+                    return new_distance;
+                }
+
+                visited[new_y][new_x] = true;
+                distance[new_y][new_x] = new_distance;
+                points.push(new_point);
+            }
+        }
+    }
+
+    return -1;
 }
 
 int main() {
