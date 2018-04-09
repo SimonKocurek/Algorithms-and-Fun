49bef9fe7ca631798fee3e1b12a45eb8c59094d0
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-04-09 20:11:35 +0200

    Is Subtree

--- tree-is-subtree.cpp
+++ tree-is-subtree.cpp
@@ -10,24 +10,52 @@ struct Node {
   Node(int val, Node* left, Node* right) : val(val), left(left), right(right) {}
 };
 
-bool Subtree(Node* root, Node* subroot) {
+string Preorder(Node* root) {
+  if (!root) {
+    return "";
+  }
 
+  return to_string(root->val) + ',' + Preorder(root->left) + ',' + Preorder(root->right);
+}
+
+bool Subtree(Node* root, Node* subroot) {
+  auto bigger = Preorder(root);
+  auto smaller = Preorder(subroot);
+
+  return bigger.find(smaller) != string::npos;
 }
 
 int main() {
-  auto root = new Node(1,
-      new Node()
-
   auto sub_root = new Node(8,
-    new Node()
+      new Node(11,
+        new Node(10, nullptr, nullptr),
+        nullptr),
+      new Node(12,
+        new Node(20,
+          new Node(22, nullptr, nullptr),
+          nullptr),
+        new Node(40, nullptr, nullptr)));
 
-  auto root2 = new Node(1;
+  auto root = new Node(8,
+      new Node(11,
+        new Node(33, nullptr, nullptr),
+        new Node(43, nullptr, nullptr)),
+      new Node(12,
+        sub_root,
+        new Node(20, nullptr, nullptr)));
 
-  auto sub_root2 = new Node(8);
-
+  auto sub_root2 = new Node(8,
+      new Node(11,
+        new Node(10, nullptr, nullptr),
+        nullptr),
+      new Node(13,
+        new Node(20,
+          new Node(22, nullptr, nullptr),
+          nullptr),
+        new Node(40, nullptr, nullptr)));
 
   cout << Subtree(root, sub_root) << "\n";
-  cout << Subtree(root2, sub_root2) << "\n";
+  cout << Subtree(root, sub_root2) << "\n";
 
   return 0;
 }
