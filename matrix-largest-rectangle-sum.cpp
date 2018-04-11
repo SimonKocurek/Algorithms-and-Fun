4cbf091802dad50ade8f466d93d3751257a59ed3
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-04-11 22:11:51 +0200

    Matrix largest sum

--- matrix-largest-rectangle-sum.cpp
+++ matrix-largest-rectangle-sum.cpp
@@ -7,14 +7,49 @@ typedef vector<Line> Matrix;
 typedef pair<int, int> Point;
 typedef pair<Point, Point> Submatrix;
 
+struct KadaneInfo {
+  int start;
+  int end;
+  int sum;
+};
+
+KadaneInfo Kadane(const Matrix& matrix, Line& sum, const int row) {
+  KadaneInfo result = {0, 0, INT_MIN};
+
+  int current_max = 0;
+  int start = 0;
+  for (int i = 0; i < matrix[0].size(); ++i) {
+    sum[i] += matrix[row][i];
+
+    current_max += sum[i];
+    if (current_max < sum[i]) {
+      current_max = sum[i];
+      start = i;
+    }
+
+    if (current_max > result.sum) {
+      result = {start, i, current_max};
+    }
+  }
+
+  return result;
+}
+
 Submatrix LargestSubmatrix(const Matrix& matrix) {
   Submatrix result = {{-1, -1}, {-1, -1}};
-  auto max = INT_MIN;
+  int max = -1;
 
   for (int i = 0; i < matrix.size(); ++i) {
-    for (int j = i; j < matrix.size(); ++j) {
+    Line sum(matrix[0].size(), 0);
 
+    for (int j = i; j < matrix.size(); ++j) {
+      auto kadane = Kadane(matrix, sum, j);
+      if (kadane.sum > max) {
+        result = {{kadane.start, i}, {kadane.end, j}};
+        max = kadane.sum;
+      }
     }
+
   }
 
   return result;
@@ -22,9 +57,17 @@ Submatrix LargestSubmatrix(const Matrix& matrix) {
 
 int main() {
   Matrix a {
+    {2, 4, 5, -2},
+    {0, 1, 3, 8},
+    {-3, -2, 1, 0},
+    {2, 5, 4, 3},
+    {2, 0, 0, 1}
   };
 
   Matrix b {
+    {-1, -3, -4},
+    {1, -3, -4},
+    {0, 0, 0}
   };
 
   Matrix c;
