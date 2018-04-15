e112212b5c3a7e57392c9e8727747ec0e941c206
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-04-15 11:29:25 +0200

    Kravicka

---
+++ palma-klavesnica.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    unordered_set<char> edge;
+};
+
+int main() {
+    int y, x, word_count;
+    cin >> y >> x >> word_count;
+
+    for (int i = 0; i < y; ++i) {
+        for (int j = 0; j < x; ++j) {
+            char c;
+            cin >> c;
+
+        }
+    }
+
+    for (int i = 0; i < word_count; ++i) {
+        
+    }
+    return 0;
+}
--- palma-kravicka.cpp
+++ palma-kravicka.cpp
@@ -13,34 +13,95 @@ const vector<Direction> directions {
     {0 , -1}
 };
 
-struct Setup {
-    Board board;
-    Direction direction;
-    Point start;
-    Point end;
-};
+bool valid(const Board& board, const int x, const int y) {
+    return x >= 0 && x < board.front().size() &&
+           y >= 0 && y < board.size() &&
+           board[y][x] != -1;
+}
 
-Setup& LoadSetup(int x, int y) {
-    Board board(y, vector<int>(x));
-    Point start;
-    Point end;
+void Fill(Board& board, Point start, Direction direction) {
+    int x = start.first;
+    int y = start.second;
 
-    for (int i = 0; i < y; ++i) {
-        for (int j = 0; j < x; ++j) {
-
+    board[y][x] = -4;
+    x += direction.first;
+    y += direction.second;
+    
+    while (valid(board, x, y)) {
+        if (board[y][x] == INT_MIN) {
+            board[y][x] = -2;
+        } else {
+            board[y][x] = 0;
         }
+
+        x += direction.first;
+        y += direction.second;
     }
 }
 
-void Solve(Setup& setup) {
-    priority_queue<Point> next;
-    next.push(setup.start);
-    for() {
+Board LoadSetup(int x, int y) {
+    Board board(y, vector<int>(x, INT_MAX));
+    Point start;
 
+    string s;
+    getline(cin, s);
+
+    Direction direction;
+    for (int i = 0; i < y; ++i) {
+        getline(cin, s);
+
+        for (int j = 0; j < x; ++j) {
+            switch (s[j]) {
+                case 'X': board[i][j] = -1; break;
+                case 'C': board[i][j] = INT_MIN; break;
+                case 'H': start = {j, i}; direction = directions[3]; break;
+                case 'P': start = {j, i}; direction = directions[0]; break;
+                case 'D': start = {j, i}; direction = directions[1]; break;
+                case 'L': start = {j, i}; direction = directions[2]; break;
+            }
+        }
+    }
+
+    Fill(board, start, direction);
+    return board;
+}
+
+void Solve(Board& board) {
+    queue<Point> next;
+
+    for (int i = 0; i < board.size(); ++i) {
+        for (int j = 0; j < board.front().size(); ++j) {
+            switch (board[i][j]) {
+                case 0: next.push({j, i}); break;
+                case -2: cout << 0 << "\n"; return;
+            }
+        }
     }
 
     while (!next.empty()) {
+        auto current = next.front();
+        auto value = board[current.second][current.first] + 1;
+        next.pop();
 
+        for (auto& direction : directions) {
+            auto x = current.first;
+            auto y = current.second;
+
+            while (valid(board, x, y)) {
+                if (board[y][x] > value) {
+                    board[y][x] = value;
+                    next.push({x, y});
+                }
+
+                if (board[y][x] == INT_MIN) {
+                    cout << value << "\n";
+                    return;
+                }
+
+                x += direction.first;
+                y += direction.second;
+            }
+        }
     }
 
     cout << -1 << "\n";
@@ -50,8 +111,8 @@ int main() {
     int y, x;
     cin >> y >> x;
 
-    auto& setup = LoadSetup(x, y);
-    Solve(setup);
+    auto board = LoadSetup(x, y);
+    Solve(board);
 
     return 0;
 }
