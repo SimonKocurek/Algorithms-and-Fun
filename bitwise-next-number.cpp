808e432402c290c455d144aebed2d1b6f7f1417a
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-04-21 20:35:43 +0200

    Next number

--- bitwise-next-number.cpp
+++ bitwise-next-number.cpp
@@ -2,26 +2,97 @@
 
 using namespace std;
 
-int NextSmaller(const int a) {
-    (a ^ (a - 1))
-    const int smallest_bit = a & (((a - 1) << 1) + 1);
-    return (a & (a - 1)) + smallest_bit;
+unsigned int NextSmaller(const unsigned int a) {
+    auto lowest_bit = a & (~(a - 1));
+    
+    if (lowest_bit == 0) {
+        throw;
+
+    } else if (lowest_bit == 1) {
+        auto result = 0;
+        for (int i = 0; i < __builtin_popcount(a); ++i) {
+            result |= (1 << i);
+        }
+
+        while ((result << 1) < a) {
+            result <<= 1;
+        }
+
+        if (result >= a) {
+            throw;
+        }
+        
+        return result;
+
+    } else {
+        return (a ^ lowest_bit) | (lowest_bit >> 1);
+    }
 }
 
-int NextBigger(const int a) {
-    const int smallest_bit = a & (((a - 1) << 1) + 1);
-    const int added = a + smallest_bit;
-    return __builtin_popcount(added) < __builtin_popcount(a) ? added + 1 : added;
+unsigned int NextBigger(const unsigned int a) {
+    if (a == UINT_MAX) {
+        throw;
+    }
+
+    auto lowest_bit = a & (~(a - 1));
+    auto result = a + lowest_bit;
+    
+    auto added = 0;
+    for (int i = __builtin_popcount(result); i < __builtin_popcount(a); ++i) {
+        added <<= 1;
+        added |= 1;
+    }
+
+    return result | added;
+}
+
+void PrintBits(const unsigned int a) {
+    for (int i = 31; i >= 0; --i) {
+        cout << ((a & (1 << i)) != 0);
+    }
+    cout << "\n";
 }
 
 int main() {
-    cout << NextSmaller(36) << "\n";
-    cout << NextSmaller(10) << "\n";
-    cout << NextSmaller(23) << "\n";
+    PrintBits(36);
+    PrintBits(NextSmaller(36));
+    PrintBits(NextBigger(36));
+    cout << "\n";
 
-    cout << NextBigger(36) << "\n";
-    cout << NextBigger(10) << "\n";
-    cout << NextBigger(23) << "\n";
+    PrintBits(10);
+    PrintBits(NextSmaller(10));
+    PrintBits(NextBigger(10));
+    cout << "\n";
+
+    PrintBits(23);
+    PrintBits(NextSmaller(23));
+    PrintBits(NextBigger(23));
+    cout << "\n";
+
+    PrintBits(0b1001111);
+    PrintBits(NextSmaller(0b1001111));
+    PrintBits(NextBigger(0b1001111));
+    cout << "\n";
+    
+    PrintBits(0b1000);
+    PrintBits(NextSmaller(0b1000));
+    PrintBits(NextBigger(0b1000));
+    cout << "\n";
+
+    PrintBits(0b100010);
+    PrintBits(NextSmaller(0b100010));
+    PrintBits(NextBigger(0b100010));
+    cout << "\n";
+
+    PrintBits(0b01001100);
+    PrintBits(NextSmaller(0b01001100));
+    PrintBits(NextBigger(0b01001100));
+    cout << "\n";
+
+    PrintBits(0b111);
+    PrintBits(NextSmaller(0b111));
+    PrintBits(NextBigger(0b111));
+    cout << "\n";
 
     return 0;
 }
