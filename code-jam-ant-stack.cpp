516b464d26a38162f63bc601cfb220c1a99cf6fb
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-05-05 12:53:38 +0200

    Ant stack

--- code-jam-ant-stack.cpp
+++ code-jam-ant-stack.cpp
@@ -11,7 +11,38 @@ void Solve() {
         cin >> ant;
     }
 
-    
+    vector<vector<int>> dp(ant_count, vector<int>(ant_count));
+    int result = 1;
+
+    for (int i = ant_count - 1; i >= 0; --i) {
+
+        int got = ants[i] * 6;
+        for (int k = i; k >= 0; --k) {
+            dp[0][k] = got;
+        }
+
+        for (int j = 1; j <= i; ++j) {
+            bool possible = false;
+            got = min(ants[i - j] * 6, dp[j - 1][i - j + 1] - ants[i - j]);
+
+            for (int k = i - j; k >= 0; --k) {
+                auto current = min(dp[j - 1][k + 1] - ants[k], ants[k] * 6);
+                got = max(current, got);
+                dp[j][k] = got;
+                if (got >= 0) {
+                    possible = true;
+                }
+            }
+
+            if (possible) {
+                result = max(result, j + 1);                
+            } else {
+                break;
+            }
+        }
+    }
+
+    cout << result;
 }
 
 int main() {
