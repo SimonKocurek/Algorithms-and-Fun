de62d3da4534d6f060ec79f10fe77b42b96c9aef
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-05-05 11:57:31 +0200

    Lollipop

--- code-jam-ant-stack.cpp
+++ code-jam-ant-stack.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 void Solve() {
+    int ant_count;
+    cin >> ant_count;
 
+    vector<int> ants(ant_count);
+    for (auto& ant : ants) {
+        cin >> ant;
+    }
+
+    
 }
 
 int main() {
--- code-jam-lollipop.cpp
+++ code-jam-lollipop.cpp
@@ -3,7 +3,42 @@
 using namespace std;
 
 void Solve() {
+    int customers;
+    cin >> customers;
 
+    vector<bool> sold(customers, false);
+    vector<int> chances(customers, 0);
+
+    for (int i = 0; i < customers; ++i) {
+        int count;
+        cin >> count;
+
+        vector<int> wants(count);
+        for (auto& want : wants) {
+            cin >> want;
+            chances[want]++;
+        }
+
+        int optimal = -1;
+        int prob = INT_MAX;
+
+        for (auto want : wants) {
+            if (sold[want]) {
+                continue;
+            }
+
+            if (chances[want] < prob) {
+                prob = chances[want];
+                optimal = want;
+            }
+        }
+
+        if (optimal > -1) {
+            sold[optimal] = true;
+        }
+
+        cout << optimal << endl;
+    }
 }
 
 int main() {
@@ -11,9 +46,7 @@ int main() {
     cin >> tests;
 
     for (int i = 1; i <= tests; ++i) {
-        cout << "Case #" << i << ": ";
         Solve();
-        cout << "\n";
     }
 
     return 0;
