92a421a331a3fdecc01b5af9b9fded6c82603108
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-05-21 22:14:25 +0200

    power set

--- backtrack-power-set.py
+++ backtrack-power-set.py
@@ -3,3 +3,23 @@
 # You may also use a list or array to represent a set.
 
 
+def power_set(input_set):
+    transformed = list(input_set)
+    result = []
+
+    def generate_index(index, added):
+        if index == len(input_set):
+            result.append(set(added))
+            return
+
+        generate_index(index + 1, added)
+        added.append(transformed[index])
+        generate_index(index + 1, added)
+        added.pop()
+
+    generate_index(0, [])
+    return result
+
+test1 = set([1, 2, 3])
+
+print(power_set(test1))
