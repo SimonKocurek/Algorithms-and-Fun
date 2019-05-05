0e7cc59f9cd4b39a9056377f824b36b71e42fe51
Simon Kocurek <simon.kocurek@gmail.com>
2019-05-06 00:00:39 +0200

    Speed up union find

--- java/Opinions.java
+++ java/Opinions.java
@@ -9,15 +9,15 @@ public class Opinions {
         int people = sc.nextInt();
         int instructions = sc.nextInt();
 
-        int[] same = new int[people + 1];
-        int[] sameSize = new int[people + 1];
+        int[] sameOpinion = new int[people + 1];
+        int[] sameOpinionSize = new int[people + 1];
 
         int[] analyzed = new int[people + 1];
         int[] analyzedSize = new int[people + 1];
 
-        for (int i = 0; i < same.length; i++) {
-            same[i] = i;
-            sameSize[i] = 1;
+        for (int i = 0; i < sameOpinion.length; i++) {
+            sameOpinion[i] = i;
+            sameOpinionSize[i] = 1;
 
             analyzed[i] = i;
             analyzedSize[i] = 1;
@@ -30,14 +30,13 @@ public class Opinions {
 
             switch (c) {
                 case "+":
-                    if (addingAnalyzed(analyzed, a, b)) {
+                    if (addingAnalyzed(analyzed, a, b) && !addingAnalyzed(sameOpinion, a, b)) {
                         System.out.println("chyba");
                         break;
                     }
 
-                    union(same, sameSize, a, b);
+                    union(sameOpinion, sameOpinionSize, a, b);
                     union(analyzed, analyzedSize, a, b);
-
                     break;
 
                 case "-":
@@ -45,18 +44,14 @@ public class Opinions {
                         System.out.println("chyba");
                         break;
                     }
-                    union(analyzed, analyzedSize, a, b);
 
+                    union(analyzed, analyzedSize, a, b);
                     break;
+
                 case "?":
                     if (!addingAnalyzed(analyzed, a, b)) {
                         System.out.println("neviem");
-                        break;
-                    }
-
-                    int rootA = find(same, a);
-                    int rootB = find(same, b);
-                    if (rootA == rootB) {
+                    } else if (addingAnalyzed(sameOpinion, a, b)) {
                         System.out.println("ano");
                     } else {
                         System.out.println("nie");
@@ -67,10 +62,10 @@ public class Opinions {
     }
 
     private static int find(int[] arr, int idx) {
-        while (arr[idx] != idx) {
-            idx = arr[idx];
+        if (arr[idx] != idx) {
+            arr[idx] = find(arr, arr[idx]);
         }
-        return idx;
+        return arr[idx];
     }
 
     private static boolean addingAnalyzed(int[] analyzed, int a, int b) {
