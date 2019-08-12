7419aed534539d8ab413b80c82fd0f4fde61f39c
Simon Kocurek <simon.kocurek@gmail.com>
2019-08-12 08:57:27 +0200

    Advent Of Code 2017-5

---
+++ python/aoc-2017-5-bonus.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+def load_instructions() -> List[int]:
+    instructions = []
+
+    line = input()
+    while line != "":
+        instructions.append(int(line))
+        line = input()
+
+    return instructions
+
+
+def instruction_steps(instructions: List[int]) -> int:
+    steps = 0
+    pointer = 0
+
+    while pointer < len(instructions):
+        move = instructions[pointer]
+
+        instructions[pointer] += 1 if move < 3 else -1
+        steps += 1
+
+        pointer += move
+
+    return steps
+
+
+def main():
+    instructions = load_instructions()
+    print(instruction_steps(instructions))
+
+
+main()
